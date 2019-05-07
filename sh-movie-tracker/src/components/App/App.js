ab18575741b5ac223969ab6d82848c2422b311e4
Sally Haefling <sallyhaefling@gmail.com>
2019-05-07 15:58:27 -0600

    Break function out of componentDidMount
    Co-authored By: Lauren Boyer <lboyer4@outlook.com>

--- sh-movie-tracker/src/components/App/App.js
+++ sh-movie-tracker/src/components/App/App.js
@@ -13,12 +13,27 @@ export default class App extends Component {
 
   componentDidMount = () => {
     const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}&language=en-US&page=1`;
-    fetchMovie(url)
-    .then(data => 
-      console.log(data)
-    )
-    
+    let movies = fetchMovie(url)
+    .then(results => this.makeMovies(results.results)
+    ) 
   }
+
+  makeMovies = (results) => {
+    console.log('results before', results)
+    results = results.map(movie => {
+      return {
+        title: movie.title,
+        image: movie.poster_path,
+        overview: movie.overview,
+        id: movie.id
+      }
+    })
+    this.setState({
+      movies: results
+    })
+  }
+
+
 
   render() {
     return (
