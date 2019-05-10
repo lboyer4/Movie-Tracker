a845e4703376b7150ce2a134f0bf43340f8cc46b
Sally Haefling <sallyhaefling@gmail.com>
2019-05-09 18:07:46 -0600

    Add fonts and style more

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Movie = ({ title, image, overview, id, favorited }) => {
   return(
     <section>
-      <h2>{title}</h2>
-      <img src={image} />
-      <p>{overview}</p>
+      <section className='movie-info-wrapper'>
+        <h2 className='movie-title'>{title}</h2>
+        <img src={image} />
+        <p>{overview}</p>
+      </section>
     </section>
   )
 }
