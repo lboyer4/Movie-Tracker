afc8707e0c7d2414e2940e129dab807a4308ce99
Sally Haefling <sallyhaefling@gmail.com>
2019-05-11 13:45:13 -0600

    Start functionality for signout
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom';
 import  Login  from '../../containers/Login/Login';
 import Signup from '../../containers/Signup/Signup';
-
 import Home from '../Home/Home';
+import { connect } from 'react-redux';
 
-
-
-export default class Header extends Component {
-
-	render() {
+const Header = (props) => {
+	let logOut = <NavLink to='/login' className="nav"> Logout </NavLink> 
+	let logIn = <NavLink to='/login' className="nav"> Login </NavLink>
+	let toggleLogin = props.loggedIn ? logOut : logIn;
 		return (
 			<header>
 				<h1 className='title'> Movie Tracker </h1>
 				<section className='link-wrapper'>
-				<NavLink to='/login' className="nav"> Login </NavLink>
-				<NavLink to='/signup' className="nav">Signup</NavLink>
+					{toggleLogin}
+					<NavLink to='/signup' className="nav">Signup</NavLink>
 				</section>
-
 			</header>
-
-
 			)
-
-
 	}
-}
+
+const mapStateToProps = (state) => ({
+	loggedIn: state.loggedIn
+})
+
+export default connect(mapStateToProps)(Header)
