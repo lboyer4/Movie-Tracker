9e7d29135182c8654ac7fc7c4d4e784d77d382d1
Lauren Boyer <lboyer4@outlook.com>
2019-05-13 19:50:42 -0600

    tested movieDetails component

---
+++ src/components/MovieDetails/MovieDetails.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MovieDetails from './MovieDetails';
+import { mockSelectedMovie } from '../../utils/mockData';
+
+describe('MovieDetails', () => {
+	it('should match snapshot without a selected movie', () => {
+		const wrapper = shallow(
+			<MovieDetails />
+		)
+		expect(wrapper).toMatchSnapshot()
+	});
+
+	it('should match snapshot with a selected move', () => {
+		const wrapper = shallow(
+			<MovieDetails
+				selectedMovie={ mockSelectedMovie }
+			/>
+		)
+	})
+})
